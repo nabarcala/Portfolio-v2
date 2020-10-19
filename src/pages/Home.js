3740a8a36cb0065fca7f7a891aabcc484c6a3fb7
Natacha <nabarcala@gmail.com>
2020-10-19 10:05:21 -0400

    Add scroll to top

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 import '../App.css';
 import About from '../components/About';
@@ -7,10 +7,32 @@ import Portfolio from '../components/Portfolio';
 import Services from '../components/Services';
 
 function Home () {
+    // Scroll to top button reference
+    const scrollRef = useRef(null);
+
+    window.addEventListener("scroll", function () {
+        // If scrolled 100px from top, see scroll button
+        if(window.pageYOffset > 100) {
+            scrollRef.current.classList.add("active");
+        } else {
+            scrollRef.current.classList.remove("active");
+        }
+    });
+
+    function smoothScroll() {
+        // may not work in all browsers
+        window.scrollTo( {
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+        // $("html, body").animate({ scrollTop: 0 }, "slow");
+    };
+
     return (
         <main className="site-main">
-            <button class="scroll-to-top" id="scroll-to-top">
-                <i class='bx bx-up-arrow-alt' ></i>
+            <button className="scroll-to-top" id="scroll-to-top" ref={scrollRef} onClick={smoothScroll}>
+                <i className='bx bx-up-arrow-alt' ></i>
             </button>
             <Banner />
             <About />
